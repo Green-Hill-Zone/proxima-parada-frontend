1673322a1ca7d07b624fdc9b5810de95bd8d589d
Vinícius  Germano <131779346+Vinigg@users.noreply.github.com>
2025-07-29 12:22:03 -0300

    Corrigindo hierarquia das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import { Footer, Header, ProtectedRoute } from './components';
 import { AuthProvider } from './contexts/AuthContext';
+import AdminPackageForm from './pages/Admin/AdminPackageForm';
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -10,55 +12,29 @@ import MyPayments from './pages/MyPayments';
 import MyTravels from './pages/MyTravels';
 import Payment from './pages/Payment';
 import Profile from './pages/Profile';
-import UserRegister from './pages/UserRegister/UserRegister';
+import { ReservationProvider } from './pages/Reservation/context/ReservationContext.tsx';
 import Reservation from './pages/Reservation/Reservation.tsx';
-import AdminPackageForm from './pages/Admin/AdminPackageForm';
+import UserRegister from './pages/UserRegister/UserRegister';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<UserRegister />} />
-          <Route path="/payment" element={<Payment />} />
-         <Route path="/reserva" element={<Reservation />} />
-          <Route path="/admin/pacotes" element={<AdminPackageForm />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/my-travels"
-            element={
-              <ProtectedRoute>
-                <MyTravels />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/my-payments"
-            element={
-              <ProtectedRoute>
-                <MyPayments />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
+        <Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<UserRegister />} />
+            <Route path="/payment" element={<Payment />} />
+            <Route path="/admin/packages" element={<AdminPackageForm />} />
+            <Route path="/reservation" element={<ReservationProvider> <Reservation /> </ReservationProvider>} />
+            <Route path="/dashboard" element={<ProtectedRoute> <Dashboard /> </ProtectedRoute>} />
+            <Route path="/profile" element={<ProtectedRoute> <Profile /> </ProtectedRoute>} />
+            <Route path="/my-travels" element={<ProtectedRoute> <MyTravels /> </ProtectedRoute>} />
+            <Route path="/my-payments" element={<ProtectedRoute> <MyPayments /> </ProtectedRoute>} />
+          </Routes>
+        </Container>
         <Footer />
       </Router>
     </AuthProvider>
